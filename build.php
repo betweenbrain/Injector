480a95e1a1e3334b6b8bc1f7e4ebfbdcbe56d3bf
Matt Thomas <matt@betweenbrain.com>
2014-11-21 05:27:24 -0500

    Get merge without a commit

--- build.php
+++ build.php
@@ -12,7 +12,7 @@ PHP_SAPI === 'cli' or die();
  */
 
 echo 'Merging develop branch' . PHP_EOL;
-exec("git merge develop");
+exec("git merge --no-commit develop");
 
 echo 'Removing README.md' . PHP_EOL;
 unlink('README.md');
