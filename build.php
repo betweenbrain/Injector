c346454f34d18599f2b9a6fe6e89a3413e1662ef
Matt Thomas <matt@betweenbrain.com>
2014-11-21 09:06:02 -0500

    Further clean up and packing of release files

--- build.php
+++ build.php
@@ -11,12 +11,60 @@ PHP_SAPI === 'cli' or die();
  * License    GNU GPL v2 or later
  */
 
+define('BASEDIR', dirname(__FILE__));
+
 echo 'Merging develop branch...' . PHP_EOL;
 exec("git merge develop");
 
 echo 'Cleaning up old release files...' . PHP_EOL;
-foreach (glob('{*.zip, *.tar.gz}', GLOB_BRACE) as $filename)
+
+// Delete old archive files
+foreach (glob('{*.zip, *.tar, *.tar.gz}', GLOB_BRACE) as $filename)
 {
 	echo 'Removing ' . $filename . PHP_EOL;
-	unlink(dirname(__FILE__) . $filename);
+	unlink(BASEDIR . $filename);
+}
+
+//
+foreach (glob(BASEDIR . '/plugins/*', GLOB_ONLYDIR) as $dir)
+{
+
+	///home/mthomas/public_html/dev/extensions/Injector/plugins/ajax/injector/injector.xml
+	$manifest = new SimpleXMLElement(file_get_contents($dir . '/injector/injector.xml'));
+
+	echo $manifest->version . PHP_EOL;
+
+	$filename = 'Injector-' . basename($dir) . '-' . $manifest->version . '.tar';
+
+	if (file_exists($filename))
+	{
+		unlink($filename);
+		echo 'Deleted ' . $filename . PHP_EOL;
+	}
+
+	if (file_exists($filename . '.gz'))
+	{
+		unlink($filename . '.gz');
+		echo 'Deleted ' . $filename . '.gz' . PHP_EOL;
+	}
+
+	try
+	{
+
+		$a = new PharData($filename);
+
+		// ADD FILES TO archive.tar FILE
+		$a->buildFromDirectory($dir . '/injector');
+
+		// COMPRESS archive.tar FILE. COMPRESSED FILE WILL BE archive.tar.gz
+		//$a->compress(Phar::GZ);
+
+		// NOTE THAT BOTH FILES WILL EXISTS. SO IF YOU WANT YOU CAN UNLINK archive.tar
+		//unlink($filename);
+	} catch (Exception $e)
+	{
+		echo "Exception : " . $e;
+	}
+
+	//echo basename($dir . PHP_EOL);
 }
